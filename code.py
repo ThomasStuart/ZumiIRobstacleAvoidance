54c75af3a0aab35d6e6231c4ee1d324be29e231c
Thoms Stuart <31195539+ThomasStuart@users.noreply.github.com>
2020-04-19 10:27:07 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,33 +1,40 @@
 from zumi.zumi import Zumi
 
-zumi = Zumi() zumi.reset_gyro()
-zumi.mpu.calibrate_MPU()
-
-
+zumi = Zumi() 
+#zumi.reset_gyro()
+#zumi.mpu.calibrate_MPU()
+POWER = 40 
+TIME  = 1 
 
 def main():
+    print("Program Started")
+    
     ir_readings = zumi.get_all_IR_data()
     bottom_right_ir = ir_readings[1] 
-    bottom_left_ir = ir_readings[3]
-    front_right_ir = ir_readings[0]
-    front_left_ir = ir_readings[5]
-    back_right_ir = ir_readings[2] 
-    back_left_ir = ir_readings[4]
+    bottom_left_ir  = ir_readings[3]
+    front_right_ir  = ir_readings[0]
+    front_left_ir   = ir_readings[5]
+    back_right_ir   = ir_readings[2] 
+    back_left_ir    = ir_readings[4]
   
   
-    min_ir_threshold_IR_front_left = 100
-    min_ir_threshold_IR_front_right = 100
+    threshold_IR_FL  = 150
+    threshold_IR_FR  = 150
   
   
     ir_readings = zumi.get_all_IR_data()
- 
-    while( front_left_ir < 162 and front_right_ir < 190 ): 
+    
+    while( True ): 
+        
         ir_reading = zumi.get_all_IR_data()
-        front_right_ir = ir_readings[0]
-        front_left_ir = ir_readings[5]
-        # zumi.reverse(40, 1)
+        front_right_ir = ir_reading[0]
+        front_left_ir  = ir_reading[5]
+        
+        if front_left_ir < threshold_IR_FL and front_right_ir < threshold_IR_FR:
+            zumi.reverse(POWER, TIME )
+   
     zumi.stop()
-
+    print("Program Ended")
 try:
     main()
 
